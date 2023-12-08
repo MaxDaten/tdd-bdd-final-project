7bf9083feb6af22f503ee931d6ad670d45a9ad55
Jan-Philip Loos <maxdaten@gmail.com>
2023-12-08 14:44:44 -0500

    Exercise 1 & 2

--- tests/factories.py
+++ tests/factories.py
@@ -31,4 +31,20 @@ class ProductFactory(factory.Factory):
         model = Product
 
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+    name = FuzzyChoice(choices=[
+        "Hat",
+        "Pants",
+        "Shirt",
+        "Apple",
+        "Banana",
+        "Pots",
+        "Towels",
+        "Ford",
+        "Chevy",
+        "Hammer",
+        "Wrench"
+    ])
+    description = factory.Faker('text', max_nb_chars=250)
+    price = FuzzyDecimal(low=0.5, high=2000.0, precision=2)
+    available = FuzzyChoice(choices=[True, False])
+    category = FuzzyChoice(choices=Category)
